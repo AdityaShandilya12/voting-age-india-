fce75d1973194399d9761536aefe8c0626075fb3
Aditya Shandilya <catchadityashandilya@gmail.com>
2024-12-12 19:53:19 +0530

    Add files via upload

---
+++ voting age.py
@@ -0,0 +1,13 @@
+#identifying of the user is ready to vote or not
+
+#taking the input
+
+age = int(input("Whats ur age!"))
+
+#condition statement
+
+if age>=18:
+    print("congrats ur elegible for voting!")
+
+else:
+    print("sorry please try again after some time")
